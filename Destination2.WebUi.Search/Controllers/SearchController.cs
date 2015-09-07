2b11a7c013e1611304bebc37301876479ed2de9d
rafiq <rafiq@holidaygems.co.uk>
2015-09-07 07:00:06 +0100

    dropdown list  updates

--- Destination2.WebUi.Search/Controllers/SearchController.cs
+++ Destination2.WebUi.Search/Controllers/SearchController.cs
@@ -7,12 +7,47 @@ using System.Linq;
 using System.ServiceModel;
 using System.Web;
 using System.Web.Mvc;
-
+using Destination2.WebUi.Search.Models;
 namespace Destination2.WebUi.Search.Controllers
 {
     public class SearchController : Controller
     {
         // GET: Search
+        [HttpPost]
+        public ActionResult Search(SearchForm model)
+        {
+            string  select = model.DepartureAirport;
+             
+            return View("Search");
+        }
+
+        public ActionResult Search()
+        {
+            SearchForm sf = new SearchForm();
+
+            List<SelectListItem> itemes = new List<SelectListItem>();
+            itemes.Add(new SelectListItem
+            {
+                Text = "LONDON",
+                Value = "LON"
+            });
+            itemes.Add(new SelectListItem
+            {
+                Text = "MANCHSTER",
+                Value = "MAN",
+                Selected = true
+            });
+            itemes.Add(new SelectListItem
+            {
+                Text = "LIVERPOOL",
+                Value = "LIV"
+            });
+        
+
+            sf.AirportList = itemes;
+            sf.AirportList2 = itemes;
+            return View(sf);
+        }
         [AcceptVerbs("POST")]
         public ActionResult ProcessSearchFormInput()
         {
@@ -23,7 +58,7 @@ namespace Destination2.WebUi.Search.Controllers
 
             // for now we are going to pretend it is a package search
             // this call will prep the search           
-            FlightServiceClient flightServiceClient = new FlightServiceClient();            
+            FlightServiceClient flightServiceClient = new FlightServiceClient();
             var flightSearchResult = flightServiceClient.StartSearch(new FlightSearch
             {
                 IsPackage = true,
@@ -38,9 +73,9 @@ namespace Destination2.WebUi.Search.Controllers
             return Redirect("search/package-flight-wait/?id=" + flightSearchResult.FlightSearchId);
         }
         [AcceptVerbs("POST")]
-        public ActionResult ProcessFlightSearch(string IsPackage, string SearchType, string DepartureAirportID, string DestinationAirportID, string DepartureDate, string ReturnDate, 
+        public ActionResult ProcessFlightSearch(string IsPackage, string SearchType, string DepartureAirportID, string DestinationAirportID, string DepartureDate, string ReturnDate,
                                                    string NumberOfAdults, string NumberOfChildren, string NumberOfInfants, string CabinClassID, string DirectFlightsOnly)
-        { 
+        {
             FlightSearch flightSearch = new FlightSearch();
             flightSearch.IsPackage = Convert.ToBoolean(IsPackage);
             flightSearch.SearchType = SearchType;
@@ -53,7 +88,7 @@ namespace Destination2.WebUi.Search.Controllers
             flightSearch.NumberOfInfants = Convert.ToInt16(NumberOfInfants);
             flightSearch.CabinClassID = Convert.ToInt16(CabinClassID);
             flightSearch.DirectFlightsOnly = Convert.ToBoolean(DirectFlightsOnly);
-            
+
             // for now we are going to pretend it is a package search
             // this call will prep the search           
             FlightServiceClient flightServiceClient = new FlightServiceClient();
---
+++ Destination2.WebUi.Search/Models/SearchForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Destination2.WebUi.Search.Models
+{
+    public class SearchForm
+    {
+        [DisplayName("Departing From")]
+        public string DepartureAirportCode { get; set; }
+
+        public string DepartureAirportText { get; set; }
+        public IEnumerable<SelectListItem> AirportList { set; get; }
+
+        [DisplayName("Going To")]
+        public string DepartureAirport { get; set; }
+
+        public IEnumerable<SelectListItem> AirportList2 { set; get; }
+
+
+    }
+}
